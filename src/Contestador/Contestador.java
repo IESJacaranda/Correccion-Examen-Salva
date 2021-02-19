bdcfff55db9cc75002b809a053bfc56b303345c9
Tequinox <txaxati2018@gmail.com>
2021-02-19 22:46:37 +0100

    Optimizada la clase Contestador y comenzado con Main

--- src/Contestador/Contestador.java
+++ src/Contestador/Contestador.java
@@ -1,64 +1,61 @@
 package Contestador;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class Contestador {
 	
 	// Mensajes de error -->
-		public static final String NO_MENSAJES = "No hay mensajes"; 
-		public static final String CONTESTADOR_LLENO = "El buzón está lleno, borre algún mensaje"; 
-		public static final String MENSAJE_NO_EXISTE = "El mensaje seleccionado no existe"; 
+		private static final String NO_MENSAJES = "No hay mensajes"; 
+		private static final String CONTESTADOR_LLENO = "El buzón está lleno, borre algún mensaje"; 
+		private static final String MENSAJE_NO_EXISTE = "El mensaje seleccionado no existe"; 
 	// <--
-	private Mensaje mensaje1;
-	private Mensaje mensaje2;
-	private Mensaje mensaje3;
+	private Mensaje mensajes[] = new Mensaje[3];
 	
 	public Contestador() {
 		super();
-		this.mensaje1 = null;
-		this.mensaje2 = null;
-		this.mensaje3 = null;
+		this.mensajes[1] = null;
+		this.mensajes[2] = null;
+		this.mensajes[3] = null;
 	}
 	
 	public void borrarMensaje1() {
-		this.mensaje1 = null;
+		this.mensajes[1] = null;
 	}
 	
 	public void borrarMensaje2() {
-		this.mensaje1 = null;
+		this.mensajes[2] = null;
 	}
 	
 	public void borrarMensaje3() {
-		this.mensaje1 = null;
+		this.mensajes[3] = null;
 	}
 	
 	public Mensaje getMensaje1() throws Exception{
-		if (mensaje1 == null) {
+		if (mensajes[1] == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
-		return mensaje1;
+		return mensajes[1];
 	}
 
 	public Mensaje getMensaje2() throws Exception{
-		if (mensaje2 == null) {
+		if (mensajes[2] == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
-		return mensaje2;
+		return mensajes[2];
 	}
 
 	public Mensaje getMensaje3() throws Exception{
-		if (mensaje3 == null) {
+		if (mensajes[3] == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
-		return mensaje3;
+		return mensajes[3];
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((mensaje1 == null) ? 0 : mensaje1.hashCode());
-		result = prime * result + ((mensaje2 == null) ? 0 : mensaje2.hashCode());
-		result = prime * result + ((mensaje3 == null) ? 0 : mensaje3.hashCode());
+		result = prime * result + Arrays.hashCode(mensajes);
 		return result;
 	}
 
@@ -71,47 +68,30 @@ public class Contestador {
 		if (getClass() != obj.getClass())
 			return false;
 		Contestador other = (Contestador) obj;
-		if (mensaje1 == null) {
-			if (other.mensaje1 != null)
-				return false;
-		} else if (!mensaje1.equals(other.mensaje1))
-			return false;
-		if (mensaje2 == null) {
-			if (other.mensaje2 != null)
-				return false;
-		} else if (!mensaje2.equals(other.mensaje2))
-			return false;
-		if (mensaje3 == null) {
-			if (other.mensaje3 != null)
-				return false;
-		} else if (!mensaje3.equals(other.mensaje3))
+		if (!Arrays.equals(mensajes, other.mensajes))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Contestador [mensaje1=" + mensaje1 + ", mensaje2=" + mensaje2 + ", mensaje3=" + mensaje3 + "]";
+		return "Mensajes del contestador, mensaje1: " + mensajes[1] + ", mensaje2: " + mensajes[2] + ", mensaje3: " + mensajes[3] + ".";
 	}
 	
 	public int numeroDeMensajes() {
 		Integer numeroMensajes = 0;
-		if (this.mensaje1 != null) {
-			numeroMensajes++;
-		}
-		if (this.mensaje2 != null) {
-			numeroMensajes++;
-		}
-		if (this.mensaje3 != null) {
-			numeroMensajes++;
+		for (Mensaje mens:mensajes) {
+			if (mens != null) {
+				numeroMensajes++;
+			}
 		}
 		return numeroMensajes;
 	}
 	
 	public void borrarTodosLosMensajes() {
-		this.mensaje1 = null;
-		this.mensaje2 = null;
-		this.mensaje3 = null;
+		this.mensajes[1] = null;
+		this.mensajes[2] = null;
+		this.mensajes[3] = null;
 	}  
 	
 	public Mensaje mensajeMasNuevo() throws Exception{
@@ -119,14 +99,10 @@ public class Contestador {
 		if (numeroDeMensajes() == 0) {
 			throw new Exception(NO_MENSAJES);
 		}
-		if ((masnuevo == null || mensaje1.getFecha().isAfter(mensajeMasNuevo().getFecha())) && mensaje1 != null) {
-			masnuevo = mensaje1;
-		}
-		if ((masnuevo == null || mensaje2.getFecha().isAfter(mensajeMasNuevo().getFecha())) && mensaje2 != null) {
-			masnuevo = mensaje2;
-		}
-		if ((masnuevo == null || mensaje3.getFecha().isAfter(mensajeMasNuevo().getFecha())) && mensaje3 != null) {
-			masnuevo = mensaje3;
+		for (Mensaje mens:mensajes) {
+			if ((masnuevo == null || mens.getFecha().isAfter(mensajeMasNuevo().getFecha())) && mens != null) {
+				masnuevo = mens;
+			}
 		}
 		return masnuevo;
 	}
@@ -135,14 +111,10 @@ public class Contestador {
 		if (numeroDeMensajes() == 0) {
 			throw new Exception(NO_MENSAJES);
 		}
-		if ((masviejo == null || mensaje1.getFecha().isBefore(mensajeMasNuevo().getFecha())) && mensaje1 != null) {
-			masviejo = mensaje1;
-		}
-		if ((masviejo == null || mensaje2.getFecha().isBefore(mensajeMasNuevo().getFecha())) && mensaje2 != null) {
-			masviejo = mensaje2;
-		}
-		if ((masviejo == null || mensaje3.getFecha().isBefore(mensajeMasNuevo().getFecha())) && mensaje3 != null) {
-			masviejo = mensaje3;
+		for (Mensaje mens:mensajes) {
+			if ((masviejo == null || mens.getFecha().isAfter(mensajeMasNuevo().getFecha())) && mens != null) {
+				masviejo = mens;
+			}
 		}
 		return masviejo;
 	}
@@ -155,37 +127,20 @@ public class Contestador {
 	}
 	public void añadirMensaje(StringBuilder remitente, StringBuilder msg) throws Exception{
 		if (this.numeroDeMensajes() == 3) {
-			throw new Exception("Buzón lleno");
+			throw new Exception(CONTESTADOR_LLENO);
 		}
-		if (this.mensaje1 == null) {
-			this.mensaje1 = new Mensaje(remitente, msg);
-		}
-		else if (this.mensaje2 == null) {
-			this.mensaje2 = new Mensaje(remitente, msg);
-		}
-		else if (this.mensaje3 == null){
-			this.mensaje3 = new Mensaje(remitente, msg);
+		for (Mensaje mens:mensajes) {
+			if (mens == null) {
+				mens = new Mensaje(remitente, msg);
+			}
 		}
 }
 	public void anadirBorrando(StringBuilder remitente, StringBuilder msg) throws Exception{
 		Mensaje masviejo = mensajeMasViejo();
-		if (this.mensaje1.equals(masviejo)) {
-			this.mensaje1 = new Mensaje(remitente, msg);
-		}
-		else if (this.mensaje2.equals(masviejo)) {
-			this.mensaje2 = new Mensaje(remitente, msg);
-		}
-		else {
-			this.mensaje3 = new Mensaje(remitente, msg);
+		for (Mensaje mens:mensajes) {
+			if (mens.equals(masviejo)) {
+				mens = new Mensaje(remitente, msg);
+			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
--- src/Contestador/Main.java
+++ src/Contestador/Main.java
@@ -1,10 +1,61 @@
 package Contestador;
 
+import java.util.Scanner;
+
 public class Main {
+	private static Scanner UserInterface = new Scanner(System.in);
+	private static Contestador Jacafit = new Contestador();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		String option = " ";
+		while (option != "exit") {
+			System.out.println(
+					   "\n 1. Consultar número de mensajes"
+					 + "\n 2. Consultar un mensaje"
+					 + "\n 3. Añadir un nuevo mensaje"
+					 + "\n 4. Añadir un nuevo mensaje borrando el más antiguo"
+					 + "\n 5. Obtener el mensaje más antiguo"
+					 + "\n 6. Obtener el mensaje más nuevo"
+					 + "\n 7. Días entre dos mensajes"
+					 + "\n 8. Borrar todos los mensajes del Contestador"
+					 + "\n 9. Salir");
+			System.out.print("Indique opción >> ");
+			try {
+				option = UserInterface.nextLine();
+				switch (option) {
+					case "1":
+						System.out.println("Hay un total de " + Jacafit.numeroDeMensajes() + " mensajes");
+						break;
+					case "2":
+						
+						break;
+					case "3":
+						
+						break;
+					case "4":
+						
+						break;
+					case "5":
+						
+						break;
+					case "6":
+						
+						break;
+					case "7":
+						
+						break;
+					case "8":
+						
+						break;
+					case "9":
+						
+						break;
+				}
+			}
+			catch (Exception e) {
+				System.out.println("Error: " + e);
+			}
+		}
 	}
 
 }
