a7f00ea74d8f1456c5bb659eb31ac59cbb4eee72
Tequinox <txaxati2018@gmail.com>
2021-02-21 10:37:46 +0100

    Terminado

--- src/Contestador/Contestador.java
+++ src/Contestador/Contestador.java
@@ -4,49 +4,54 @@ import static java.time.temporal.ChronoUnit.DAYS;
 import java.time.LocalDateTime;
 
 public class Contestador {
-		private static final LocalDateTime FECHA_MAXIMA = LocalDateTime.of(2999, 12, 12, 12, 12);
-		private static final LocalDateTime FECHA_MINIMA = LocalDateTime.of(1970, 12, 12, 12, 12);
-	// Mensajes de error -->
+		private static final LocalDateTime FECHA_MAXIMA = LocalDateTime.of(2999, 12, 12, 12, 12);		// Fecha para obtener el mensaje más antiguo 
+		private static final LocalDateTime FECHA_MINIMA = LocalDateTime.of(1970, 12, 12, 12, 12);		// Fecha para obtener el mensaje más nuevo
+		
+	// <<<<<<<<<<<<<<<<<<-------------------------------- Mensajes de error --------------------------------------->>>>>>>>>>
+		
 		private static final String NO_MENSAJES = "No hay mensajes"; 
 		private static final String CONTESTADOR_LLENO = "El buzón está lleno, borre algún mensaje"; 
 		private static final String MENSAJE_NO_EXISTE = "El mensaje seleccionado no existe"; 
+		
+	// <<<<<<<<<<<<<<<<<<------------------------------------- Mensajes -------------------------------------------->>>>>>>>>>
+		
 		private Mensaje mensaje1,mensaje2,mensaje3;
-	// <--
+	// <<<<<<<<<<<<<<<<<------------------------------------- Métodos ----------------------------------------------->>>>>>>>>>
 	
-	public Contestador() {
+	public Contestador() {									// Constructor : Establece todos los mensajes a null
 		super();
 		this.mensaje1 = null;
 		this.mensaje2 = null;
 		this.mensaje3 = null;
 	}
 	
-	public void borrarMensaje1() {
+	public void borrarMensaje1() {							// Borrar mensaje1 : Establece el mensaje 1 en null
 		this.mensaje1 = null;
 	}
 	
-	public void borrarMensaje2() {
+	public void borrarMensaje2() {							// Borrar mensaje2 : Establece el mensaje 2 en null
 		this.mensaje2 = null;
 	}
 	
-	public void borrarMensaje3() {
+	public void borrarMensaje3() {							// Borrar mensaje3 : Establece el mensaje 3 en null
 		this.mensaje3 = null;
 	}
 	
-	public Mensaje getMensaje1() throws Exception{
+	public Mensaje getMensaje1() throws Exception{			// Obtener mensaje1 : Obtiene el mensaje 1, si es null lanza una excepción
 		if (mensaje1 == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
 		return mensaje1;
 	}
 
-	public Mensaje getMensaje2() throws Exception{
+	public Mensaje getMensaje2() throws Exception{			// Obtener mensaje2 : Obtiene el mensaje 2, si es null lanza una excepción
 		if (mensaje2 == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
 		return mensaje2;
 	}
 
-	public Mensaje getMensaje3() throws Exception{
+	public Mensaje getMensaje3() throws Exception{			// Obtener mensaje3 : Obtiene el mensaje 3, si es null lanza una excepción
 		if (mensaje3 == null) {
 			throw new Exception(MENSAJE_NO_EXISTE);
 		}
@@ -54,7 +59,7 @@ public class Contestador {
 	}
 
 	@Override
-	public int hashCode() {
+	public int hashCode() {									// hashcode y equals
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((mensaje1 == null) ? 0 : mensaje1.hashCode());
@@ -91,89 +96,89 @@ public class Contestador {
 	}
 
 	@Override
-	public String toString() {
+	public String toString() {									// ToString
 		return "Mensajes del contestador, mensaje1: " + mensaje1 + ", mensaje2: " + mensaje2 + ", mensaje3: " + mensaje3 + ".";
 	}
 	
-	public int numeroDeMensajes() {
-		Integer numeroMensajes = 0;
-		if (mensaje1 != null) {
+	public int numeroDeMensajes() {								// Número de mensajes
+		Integer numeroMensajes = 0;									// Define un contador
+		if (mensaje1 != null) {										// Si el mensaje 1 no es null incrementamos el contador
 			numeroMensajes++;
 		}
-		if (mensaje2 != null) {
+		if (mensaje2 != null) {										// Si el mensaje 2 no es null incrementamos el contador
 			numeroMensajes++;
 		}
-		if (mensaje3 != null) {
+		if (mensaje3 != null) {										// Si el mensaje 3 no es nul incrementamos el contador
 			numeroMensajes++;
 		}
-		return numeroMensajes;
+		return numeroMensajes;										// Retornamos el valor del contador
 	}
 	
-	public void borrarTodosLosMensajes() {
+	public void borrarTodosLosMensajes() {						// Borrar todos los mensajes: Establece todos los mensajes a null
 		this.mensaje1 = null;
 		this.mensaje2 = null;
 		this.mensaje3 = null;
 	}  
 	
-	public Mensaje mensajeMasNuevo() throws Exception{
-		Mensaje masnuevo = null;
-		if (numeroDeMensajes() == 0) {
+	public Mensaje mensajeMasNuevo() throws Exception{			// Mensaje más nuevo
+		Mensaje masnuevo = null;									// Crea un nuevo mensaje a nulo que almacenará el mensaje más nuevo
+		if (numeroDeMensajes() == 0) {								// Comprobamos si hay al menos un mensaje y en el caso de que no haya lanzamos excepción
 			throw new Exception(NO_MENSAJES);
 		}
-		if (mensaje1 != null && (masnuevo == null || mensaje1.getFecha().isAfter(FECHA_MINIMA))) {
+		if (mensaje1 != null && (masnuevo == null || mensaje1.getFecha().isAfter(FECHA_MINIMA))) {		// Si el mensaje 1 no es nulo y (el mas nuevo es nulo o es más nuevo que el que tenemos almacenado) lo establecemos como mensaje más nuevo
 			masnuevo = mensaje1;
 		}
-		if (mensaje2 != null && (masnuevo == null || mensaje2.getFecha().isAfter(FECHA_MINIMA))) {
+		if (mensaje2 != null && (masnuevo == null || mensaje2.getFecha().isAfter(FECHA_MINIMA))) {		// Si el mensaje 2 no es nulo y (el mas nuevo es nulo o es más nuevo que el que tenemos almacenado) lo establecemos como mensaje más nuevo
 			masnuevo = mensaje2;
 		}
-		if (mensaje3 != null && (masnuevo == null || mensaje3.getFecha().isAfter(FECHA_MINIMA))) {
+		if (mensaje3 != null && (masnuevo == null || mensaje3.getFecha().isAfter(FECHA_MINIMA))) {		// Si el mensaje 3 no es nulo y (el mas nuevo es nulo o es más nuevo que el que tenemos almacenado) lo establecemos como mensaje más nuevo
 			masnuevo = mensaje3;
 		}
-		return masnuevo;
+		return masnuevo;																				// Retornamos el mensaje más nuevo
 	}
-	public Mensaje mensajeMasViejo() throws Exception{
-		Mensaje masviejo = null;
-		if (numeroDeMensajes() == 0) {
+	public Mensaje mensajeMasViejo() throws Exception{									// Mensaje más viejo
+		Mensaje masviejo = null;															// Creamos un nuevo mensaje a null que almacenará el mensaje más viejo
+		if (numeroDeMensajes() == 0) {														// Si el número de mensajes en 0 lanzamos una excepción indicando que no hay mensajes
 			throw new Exception(NO_MENSAJES);
 		}
-			if (mensaje1 != null && (masviejo == null || mensaje1.getFecha().isAfter(FECHA_MAXIMA))) {
+			if (mensaje1 != null && (masviejo == null || mensaje1.getFecha().isAfter(FECHA_MAXIMA))) {	// Si el mensaje 1 no es nulo y (el mas viejo es nulo o es más viejo que el que tenemos almacenado) lo establecemos como mensaje más viejo
 				masviejo = mensaje1;
 			}
-			if (mensaje2 != null && (masviejo == null || mensaje2.getFecha().isAfter(FECHA_MAXIMA))) {
+			if (mensaje2 != null && (masviejo == null || mensaje2.getFecha().isAfter(FECHA_MAXIMA))) {	// Si el mensaje 2 no es nulo y (el mas viejo es nulo o es más viejo que el que tenemos almacenado) lo establecemos como mensaje más viejo
 				masviejo = mensaje2;
 			}
-			if (mensaje3 != null && (masviejo == null || mensaje3.getFecha().isAfter(FECHA_MAXIMA))) {
-				masviejo = mensaje3;
+			if (mensaje3 != null && (masviejo == null || mensaje3.getFecha().isAfter(FECHA_MAXIMA))) {	// Si el mensaje 3 no es nulo y (el mas viejo es nulo o es más viejo que el que tenemos almacenado) lo establecemos como mensaje más viejo
+				masviejo = mensaje3;														// Retornamos el mensaje más viejo
 			}
 		return masviejo;
 	}
-	public int diasEntreMensajes() throws Exception {
-		long numDias = DAYS.between(mensajeMasNuevo().getFecha(), mensajeMasNuevo().getFecha());
-		return (int) numDias;
+	public int diasEntreMensajes() throws Exception {											// Días entre mensajes
+		long numDias = DAYS.between(mensajeMasNuevo().getFecha(), mensajeMasNuevo().getFecha()); // Creamos una variable de tipo long la cual almacenará los días entre las dos fechas, para obtener la fecha usamos la funcion DAYS.between a la cual le pasamos la fecha del mensaje mas nuevo y la fecha del mensaje más antiguo 
+		return (int) numDias;																		// Retornamos el resultado como un entero
 	}
-	public void añadirMensaje(StringBuilder remitente, StringBuilder msg) throws Exception{
-		if (this.numeroDeMensajes() == 3) {
+	public void añadirMensaje(StringBuilder remitente, StringBuilder msg) throws Exception{		// Añadir mensaje
+		if (this.numeroDeMensajes() == 3) {															// Comprueba si el buzon está lleno, en el caso de que sí lanzamos una excepción
 			throw new Exception(CONTESTADOR_LLENO);
 		}
-		if (mensaje1 == null) {
+		if (mensaje1 == null) {																	// Si el mensaje 1 es nulo lo guardamos ahí
 			mensaje1 = new Mensaje(remitente, msg);
 		}
-		else if (mensaje2 == null) {
+		else if (mensaje2 == null) {															// Si no lo guardamos el mensaje2
 			mensaje2 = new Mensaje(remitente, msg);
 		}
 		else {
-			mensaje3 = new Mensaje(remitente, msg);
+			mensaje3 = new Mensaje(remitente, msg);												// Si no en mensaje3
 		}
 }
-	public void anadirBorrando(StringBuilder remitente, StringBuilder msg) throws Exception{
-		Mensaje masviejo = mensajeMasViejo();
-		if (mensaje1.equals(masviejo)) {
+	public void anadirBorrando(StringBuilder remitente, StringBuilder msg) throws Exception{	// Añadir borrando
+		Mensaje masviejo = mensajeMasViejo();														// Creamos un nuevo Mensaje el cual almacenará el mensaje más viejo 
+		if (mensaje1.equals(masviejo)) {															// Comprobamos si el mensaje1 es el más viejo y si es así lo almacenamos
 			mensaje1 = new Mensaje(remitente, msg);
 		}
-		else if (mensaje2.equals(masviejo)) {
+		else if (mensaje2.equals(masviejo)) {														// Comprobamos si el mensaje2 es el más viejo y si es así lo almacenamos
 			mensaje2 = new Mensaje(remitente, msg);
 		}
-		else {
+		else {																						// Comprobamos si el mensaje3 es el más viejo y si es así lo almacenamos
 			mensaje3 = new Mensaje(remitente, msg);
 		}
 	}
--- src/Contestador/Main.java
+++ src/Contestador/Main.java
@@ -30,11 +30,11 @@ public class Main {
 		
 		StringBuilder option = new StringBuilder(" ");
 		StringBuilder option2 = new StringBuilder(" ");
+		boolean exitApp = false;
 		
 		// Definimos un Bucle el cual mantendrá en ejecución nuestra aplicación hasta que el usuario desee salir
 		
-		while (!option.equals("exit")) {
-			
+		while (!exitApp) {
 			// Imprimimos el menú y el selector
 			
 			System.out.println(
@@ -59,100 +59,101 @@ public class Main {
 				// Con un switch escaneamos la opción elegida por el usuario
 				
 				switch (option.toString()) {
-					case "1":
-						System.out.println("Hay un total de " + Jacafit.numeroDeMensajes() + " mensajes");
+					case "1":													// Total de mensajes
+						System.out.println("Hay un total de " + Jacafit.numeroDeMensajes() + " mensajes");	// Llamamos al método numero de mensajes y obtenemos e imprimimos el número de mensajes
 						break;
-					case "2":
-						System.out.println(MENSAJES);
-						System.out.print(SELECT_MSG);
-						option = new StringBuilder(UserInterface.nextLine());
-						switch (option.toString()) {
-							case "1":
-								Jacafit.getMensaje1();
-								option = oirHablarSelect();
-								switch(option.toString()) {
-									case "1":
+					case "2":													// Consultar mensaje
+						System.out.println(MENSAJES);									// Imprimimos el menú mensajes
+						System.out.print(SELECT_MSG);									// Mostramos el selector
+						option = new StringBuilder(UserInterface.nextLine());			// Escaneamos la entrada del usuario
+						switch (option.toString()) {									// Comprobamos que ha puesto el usuario
+							case "1":												// En el caso de que sea 1
+								Jacafit.getMensaje1();									// Llamamos al metodo get mensaje solo con el fin de que si este es null nos lance la excepción y no le muestre al usuario el menú oir / borrar
+								option = oirHablarSelect();								// Llamamos a la función seleccionar oir o hablar 
+								switch(option.toString()) {								// Verificamos que ha insertado el usuario
+									case "1":												// En el caso de que haya seleccionado 1 imprimimos ahora si el mensaje
 										System.out.println(Jacafit.getMensaje1());
 										break;
-									case "2":
+									case "2":												// En el caso de que haya seleccionado 2 imprimimos el mensaje que se va a borrar y acto seguido lo borramos
 										System.out.println(DELETING_MSG + Jacafit.getMensaje1());
 										Jacafit.borrarMensaje1();
 										break;
-									default:
+									default:												// En el caso de que se haya equivocado lanzamos una excepción
 										throw new Exception(OPT_INCORR);
-								}
+								}// Si ha seleccionado una opción no disponible lanzamos excepción
 								break;
-							case "2":
-								Jacafit.getMensaje2();
-								option = oirHablarSelect();
-								switch(option.toString()) {
-									case "1":
-										System.out.println(Jacafit.getMensaje2());
+							case "2":												// En el caso de que sea 2
+								Jacafit.getMensaje2();									// Llamamos al metodo get mensaje solo con el fin de que si este es null nos lance la excepción y no le muestre al usuario el menú oir / borrar
+								option = oirHablarSelect();								// Llamamos a la función seleccionar oir / hablar
+								switch(option.toString()) {								// Comprobamos que ha insertado el usuario
+									case "1":												// En el caso de que sea 1
+										System.out.println(Jacafit.getMensaje2());				// Imprimimos el mensaje
 										break;
 									case "2":
-										System.out.println(DELETING_MSG + Jacafit.getMensaje2());
+										System.out.println(DELETING_MSG + Jacafit.getMensaje2());	// Imprimimos y acto seguido lo borramos
 										Jacafit.borrarMensaje2();
 										break;
-									default:
+									default:													// Si ha seleccionado una opción incorrecta lanzamos excepción
 										throw new Exception(OPT_INCORR);
 								}
 								break;
-							case "3":
-								Jacafit.getMensaje2();
-								option = oirHablarSelect();
-								switch(option.toString()) {
-									case "1":
+							case "3":												// En el caso de que sea 3
+								Jacafit.getMensaje2();									// Llamamos al metodo get mensaje solo con el fin de que si este es null nos lance la excepción y no le muestre al usuario el menú oir / borrar
+								option = oirHablarSelect();								// Llamamos a la función seleccionar oir / hablar
+								switch(option.toString()) {							// Escaneamos la entrada del usuario
+									case "1":											// Si ha seleccionado 1 imprimimos el mensaje
 										System.out.println(Jacafit.getMensaje3());
 										break;
-									case "2":
+									case "2":											// Si ha seleccionado 2 imprimimos el mensaje a borrar y lo borramos
 										System.out.println(DELETING_MSG + Jacafit.getMensaje3());
 										Jacafit.borrarMensaje3();
 										break;
-									default:
+									default:											// Si ha seleccionado una opción no disponible lanzamos excepción
 										throw new Exception(OPT_INCORR);
 								}
 								break;
-							default:
+							default:													// Si ha seleccionado una opción no disponible lanzamos excepción
 								throw new Exception(OPT_INCORR);
 						}
 						break;
-					case "3":
-						if (Jacafit.numeroDeMensajes() == 3) {
+					case "3":											// Añadir mensaje
+						if (Jacafit.numeroDeMensajes() == 3) {								// En el caso de que el contestador esté lleno nos lanzará un excepción indicandolo
 							throw new Exception(CONTESTADOR_FULL);
 						}
 						else {
-							option = selectUser();
-							option2 = selectMsg();
-							Jacafit.añadirMensaje(option, option2);
-							System.out.println(MSG_APPEND);
+							option = selectUser();											// Llama a la función seleccionar usuario
+							option2 = selectMsg();											// Llama a la función seleccionar mensaje 
+							Jacafit.añadirMensaje(option, option2);							// Al metodo añadir le pasamos los datos anteriores
+							System.out.println(MSG_APPEND);									// Indicamos que el mensaje se ha añadido con éxito
 						}
 						break;
-					case "4":
-						option = selectUser();
-						option2 = selectMsg();
-						Jacafit.anadirBorrando(option, option2);
-						System.out.println(MSG_APPEND);
+					case "4":											// Añadir mensaje borrando
+						option = selectUser();												// Llama a la función seleccionar usuari
+						option2 = selectMsg();												// Llama a la función seleccionar mensaje
+						Jacafit.anadirBorrando(option, option2);							// Al metodo añadirborrando le pasamos los datos anteriores
+						System.out.println(MSG_APPEND);										// Indicamos que el mensaje se ha añadido con éxito
 						break;
-					case "5":
-						System.out.println(Jacafit.mensajeMasViejo());
+					case "5":											// Obtener el mensaje más antiguo
+						System.out.println(Jacafit.mensajeMasViejo());						// Obtiene e imprime el mensaje más viejo
 						break;
-					case "6":
-						System.out.println(Jacafit.mensajeMasNuevo());
+					case "6":											// Obtener el mensaje más nuevo
+						System.out.println(Jacafit.mensajeMasNuevo());						// Obtiene e imprime el mensaje más nuevo
 						break;
-					case "7":
-						System.out.println("Ha pasado " + Jacafit.diasEntreMensajes() + " días entre el mensaje más nuevo y el más antiguo");
+					case "7":											// Obtener el número de días entre mensaje nuevo y viejo
+						System.out.println("Ha pasado " + Jacafit.diasEntreMensajes() + " días entre el mensaje más nuevo y el más antiguo");	// Imprime el número de días que ha pasado entre el mensaje más nuevo y el mensaje más antiguo
 						break;
-					case "8":
-						Jacafit.borrarTodosLosMensajes();
-						System.out.println("Todos los mensajes han sido borrados");
+					case "8":											// Borrar todos los mensajes
+						Jacafit.borrarTodosLosMensajes();									// Borra todos los mensajes
+						System.out.println("Todos los mensajes han sido borrados");			// Indicamos que se han borrado todos los mensajes
 						break;
-					case "9":
-						salir();
-					default:
-						throw new Exception(OPT_INCORR);
+					case "9":											// Salir
+						exitApp = salir();															// Llama a la función salir
+						break;
+					default:											// Error
+						throw new Exception(OPT_INCORR);									// En el caso de que elija una opción incorrecta se mostrará el mensaje de opción incorrecta
 				}
 			}
-			catch (Exception e) {
+			catch (Exception e) {															// En el caso de que haya un excepción se imprimirá por pantalla el texto Error: seguido del error producido
 				System.out.println("Error: " + e);
 			}
 		}
@@ -177,15 +178,14 @@ public class Main {
 		return new StringBuilder(UserInterface.nextLine());
 	}
 
-	private static void salir() throws Exception {							// Función salir de la aplicación
+	private static boolean salir() throws Exception {							// Función salir de la aplicación
 		System.out.print(ESTA_SEGURO);
-		StringBuilder option = new StringBuilder(UserInterface.nextLine());
-		switch (option.toString().toUpperCase()) {
+		String selection = UserInterface.nextLine().toUpperCase();
+		switch (selection) {
 			case "S":
-				option = new StringBuilder("exit");
-				break;
+				return true;
 			case "N":
-				break;
+				return false;
 			default:
 				throw new Exception(OPT_INCORR);
 		}
